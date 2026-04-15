b1bacb97427cf3fc554b2d789ee712958d3bb6a1
maf1250 <alfayez1250@yahoo.com>
2026-04-15 13:02:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminPage from "./admin/AdminPage";
 import TraineePortalPage from "./trainee/TraineePortalPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/trainee/:slug" element={<TraineePortalPage />} />
         <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
