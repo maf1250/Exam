3d5c74055032a1b07d82461563f50c0830c65050
maf1250 <alfayez1250@yahoo.com>
2026-04-16 18:07:55 +0300

    Update AdminPage.jsx

--- src/admin/AdminPage.jsx
+++ src/admin/AdminPage.jsx
@@ -40,7 +40,7 @@ function CountUp({ end, duration = 1000 }) {
 
     requestAnimationFrame(animate);
   }, [end, duration]);
-
+return <span>{count)}</span>;
 }
 function normalizeArabicLetters(value) {
   return String(value ?? "")
