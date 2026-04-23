8a1da6134afd9b56cabcbc2e01e161a2d8ccfc86
maf1250 <alfayez1250@yahoo.com>
2026-04-23 22:56:40 +0300

    Update AdminPage.jsx

--- src/admin/AdminPage.jsx
+++ src/admin/AdminPage.jsx
@@ -1,4 +1,4 @@
-=import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import Papa from "papaparse";
 import JSZip from "jszip";
 import CollegeJsonUploader from "../components/CollegeJsonUploader";
