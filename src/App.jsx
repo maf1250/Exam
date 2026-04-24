1653171b2b39d134a43578c885af6df3b43f4637
maf1250 <alfayez1250@yahoo.com>
2026-04-24 11:34:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/admin" replace />} />
+        <Route path="/" element={<TraineePortalPage />} /> {/* <Navigate to="/admin" replace /> */}
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/trainee/:slug" element={<TraineePortalPage />} />
         <Route path="/:slug" element={<TraineePortalPage />} />
-        <Route path="*" element={<Navigate to="/admin" replace />} />
+        <Route path="*" element={<TraineePortalPage />} /> {/* <Navigate to="/admin" replace /> */}
       </Routes>
     </HashRouter>
   );
