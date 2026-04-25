57ec6369870114ae481140f2256900096a58643c
maf1250 <alfayez1250@yahoo.com>
2026-04-25 16:55:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminPage from "./admin/AdminPage";
 import TraineePortalPage from "./trainee/TraineePortalPage";
 
+function HashRedirect() {
+  useEffect(() => {
+    if (window.location.hash.startsWith("#/")) {
+      const path = window.location.hash.replace("#", "");
+      window.history.replaceState(null, "", path);
+    }
+  }, []);
+
+  return null;
+}
+
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <HashRedirect />
+
       <Routes>
-        <Route path="/" element={<TraineePortalPage />} /> {/* <Navigate to="/admin" replace /> */}
+        <Route path="/" element={<TraineePortalPage />} />
         <Route path="/sportal" element={<AdminPage />} />
         <Route path="/trainee/:slug" element={<TraineePortalPage />} />
         <Route path="/:slug" element={<TraineePortalPage />} />
-        <Route path="*" element={<TraineePortalPage />} /> {/* <Navigate to="/admin" replace /> */}
-         <Route path="*/:slug" element={<TraineePortalPage />} />
+        <Route path="*" element={<TraineePortalPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
